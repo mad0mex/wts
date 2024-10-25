55bfde4938473d95755a45dcee87c4c4a5fdbc72
mad0mex <kriegl.max@gmail.com>
2024-10-25 17:26:24 +0200

    Update scripts.js

---
+++ scripts.js
@@ -0,0 +1,11 @@
+document.getElementById('changeTextButton').addEventListener('click', function() {
+    document.getElementById('text').innerText = 'Text wurde durch Button-Klick geändert!';
+});
+
+document.getElementById('hoverDiv').addEventListener('mouseover', function() {
+    document.getElementById('text').innerText = 'Text wurde durch Mausbewegung geändert!';
+});
+
+document.getElementById('inputField').addEventListener('keydown', function() {
+    document.getElementById('text').innerText = 'Text wurde durch Tastatureingabe geändert!';
+});
